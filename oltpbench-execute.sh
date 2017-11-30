737237db8a54755a58560a29ab5074a991908a98
Lukas Werling <lukas.werling@gmail.com>
2017-11-30 13:26:55 +0100

    Add config for executing on remote MySQL

--- oltpbench-execute.sh
+++ oltpbench-execute.sh
@@ -7,5 +7,5 @@ scriptdir=`pwd`
 export LC_ALL=C
 
 cd ../oltpbench
-./oltpbenchmark -b tpcc -c "$scriptdir/sample_tpcc_config.xml" --execute=true
+./oltpbenchmark -b tpcc -c "$scriptdir/sample_tpcc_config_i30pc21.xml" --execute=true
 
